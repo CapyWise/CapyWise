c0029293bbb129dc0ba232d743820d85c2607826
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2025-03-04 23:41:01 -0800

    Moved 'Get Started' button and worked on spacing in Landing Page

--- capywise/lib/views/auth/landing_screen.dart
+++ capywise/lib/views/auth/landing_screen.dart
@@ -5,121 +5,129 @@ class LandingPageFinal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          // Login Button
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-            decoration: BoxDecoration(
-              color: const Color(0xFF271908),
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: const Text(
-              'Login',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontFamily: 'Poppins',
+    return Container(
+      color: const Color(0xFFE8D9D6), // Set background color here
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Top Padding
+            const SizedBox(height:40),
+
+            // Login Button
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
+              decoration: BoxDecoration(
+                color: const Color(0xFF271908),
+                borderRadius: BorderRadius.circular(40),
               ),
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          // Image
-          Container(
-            width: 360,
-            height: 360,
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: NetworkImage(""),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(20),
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          // Title
-          const Text(
-            'CapyWise',
-            style: TextStyle(
-              color: Color(0xFF271908),
-              fontSize: 64,
-              fontFamily: 'Poppins',
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 20),
-
-          // Description
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 400),
-            child: Text(
-              'We are CapyWise, a team of passionate students dedicated to simplifying academic scheduling. Our goal is to help students stay organized and stress-free with an all-in-one calendar solution!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color(0xFF252525),
-                fontSize: 18,
-                fontFamily: 'Poppins',
-              ),
-            ),
-          ),
-          const SizedBox(height: 40),
-
-          // Features Section
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FeatureCard(
-                  title: 'Calendar',
-                  description: 'Keep track of your daily schedule, assignments, and events all in one place.',
-                  imageUrl: '',
-                  color: Color(0xFFDDAF82),
+              child: const Text(
+                'Login',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                  fontFamily: 'Poppins',
                 ),
-                FeatureCard(
-                  title: 'Exam Scheduler',
-                  description: 'Plan and manage your final exams effortlessly to avoid last-minute stress.',
-                  imageUrl: '',
-                  color: Color(0xFFFDF5D5),
-                ),
-                FeatureCard(
-                  title: 'Reminders',
-                  description: 'Set notifications for important deadlines, exams, and tasks to stay on top of your academic life.',
-                  imageUrl: '',
-                  color: Color(0xFFEA6D61),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 40),
-
-          // Get Started Button
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-            decoration: BoxDecoration(
-              color: const Color(0xFF271908),
-              borderRadius: BorderRadius.circular(40),
-            ),
-            child: const Text(
-              'Get Started',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 22,
-                fontFamily: 'Poppins',
               ),
             ),
-          ),
-          const SizedBox(height: 20)
-        ],
+            const SizedBox(height: 20),
+
+            // Image
+            Container(
+              width: 360,
+              height: 360,
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: NetworkImage(""),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Title
+            const Text(
+              'CapyWise',
+              style: TextStyle(
+                color: Color(0xFF271908),
+                fontSize: 64,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            // Description
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 400),
+              child: Text(
+                'We are CapyWise, a team of passionate students dedicated to simplifying academic scheduling. Our goal is to help students stay organized and stress-free with an all-in-one calendar solution!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFF252525),
+                  fontSize: 18,
+                  fontFamily: 'Poppins',
+                  height: 2,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            // Get Started Button
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: const Text(
+                'Get Started',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+
+            // Features Section
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FeatureCard(
+                    title: 'Calendar',
+                    description: 'Keep track of your daily schedule, assignments, and events all in one place.',
+                    imageUrl: '',
+                    color: Color(0xFFDDAF82),
+                  ),
+                  FeatureCard(
+                    title: 'Exam Scheduler',
+                    description: 'Plan and manage your final exams effortlessly to avoid last-minute stress.',
+                    imageUrl: '',
+                    color: Color(0xFFFDF5D5),
+                  ),
+                  FeatureCard(
+                    title: 'Reminders',
+                    description: 'Set notifications for important deadlines, exams, and tasks to stay on top of your academic life.',
+                    imageUrl: '',
+                    color: Color(0xFFEA6D61),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 40),
+          ],
+        ),
       ),
     );
   }
 }
+
 
 class FeatureCard extends StatelessWidget {
   final String title;
@@ -175,6 +183,7 @@ class FeatureCard extends StatelessWidget {
               color: Color(0xFF252525),
               fontSize: 16,
               fontFamily: 'Poppins',
+              height: 1.5,
             ),
           ),
         ],
