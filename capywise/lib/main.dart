58c695cabf37cd852349264c3d75a2f520912b08
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2025-03-08 14:08:15 -0800

    Update capywise/lib/main.dart
    Co-authored-by: Izabel Smid ૮꒰ ˶• ༝ •˶꒱ა ♡ <80926970+izabellauren789@users.noreply.github.com>

--- capywise/lib/main.dart
+++ capywise/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'core/base_page.dart';
 import 'views/auth/landing_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
